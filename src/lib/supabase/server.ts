4057cafe755705aff47e9a6d67b06748f24dcd74
Tobani Esan-George <tobanigeorge@gmail.com>
2025-10-24 14:59:26 +0100

    fixes & remove irrelevant templates

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -2,8 +2,8 @@ import { env } from "@/env";
 import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
-export const createSupabaseServerClient = () => {
-  const cookieStore = cookies();
+export const createSupabaseServerClient = async () => {
+  const cookieStore = await cookies();
 
   return createServerClient(
     env.NEXT_PUBLIC_SUPABASE_URL,
